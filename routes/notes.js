58a40ff9bed0740cd90515cec37fcb01b5904bbd
abi <abimael.monarrez58@gmail.com>
2022-01-31 17:37:42 -0700

    handle DELETE requests

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const notes = require("express").Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require("../helpers/fsUtils")
+const { readAndAppend, readFromFile, writeToFile } = require("../helpers/fsUtils")
 
 notes.post("/", (req, res) => {
     if(req.body) {
@@ -20,5 +20,18 @@ notes.post("/", (req, res) => {
 notes.get("/", (req, res) => {
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
+
+notes.delete("/:id", (req, res) => {
+    const param = req.params.id;
+
+    readFromFile("./db/db.json")
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+          const result = json.filter((note) => note.id !== param);
+          writeToFile("./db/db.json", result);
+          res.json(`Note ${param} has been deleted 🗑`);
+      });
+
+})
 
 module.exports = notes;
